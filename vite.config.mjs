c6a3ec0eb3c57883d87718c3737426dae07116fd
Shanjid Ahmed Rafi <shanjidahmed66@gmail.com>
2025-09-30 20:43:43 +0600

    djfldk

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
-})
+});
+
